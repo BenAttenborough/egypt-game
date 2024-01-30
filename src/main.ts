834f59c0bbbf4ff191671f29da8ca3d732967a47
Ben Attenborough <battenborough@gmail.com>
2024-01-30 22:55:39 +0000

    Work on footprints

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,20 @@ import "./style.css";
 import { Grid } from "./grid";
 import playerImgUrl from "./player-sheet.png";
 import tomb1ImgUrl from "./tomb-1.png";
+import footImgUrl from "./foot.png";
+import bigFootRightImgUrl from "./bigfoot-right.png";
+import tlUrl from "./topleft.png";
+import trUrl from "./topright.png";
+import blUrl from "./bottomleft.png";
+import brUrl from "./bottomright.png";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
     <canvas id="game-canvas" width="670" height="320"></canvas>
   </div>
 `;
+
+type Point = [number, number];
 
 type GameConfig = {
   stopMain: number;
@@ -29,6 +37,22 @@ player.src = playerImgUrl;
 
 let tomb1 = new Image();
 tomb1.src = tomb1ImgUrl;
+
+let foot = new Image();
+foot.src = footImgUrl;
+
+let footR = new Image();
+footR.src = bigFootRightImgUrl;
+
+let tl = new Image();
+tl.src = tlUrl;
+
+let tr = new Image();
+tr.src = trUrl;
+let bl = new Image();
+bl.src = blUrl;
+let br = new Image();
+br.src = brUrl;
 
 function doubleArray(arr: number[]): number[] {
   return arr.reduce((prev: number[], cur) => {
@@ -89,6 +113,48 @@ function drawPlayer(direction: Direction) {
   }
 }
 
+type Quadrant = "TL" | "TR" | "BL" | "BR";
+
+function drawQuarterImage(
+  image: HTMLImageElement,
+  quadrant: Quadrant,
+  position: Point,
+  quadSize: number
+) {
+  let offSetX = 0;
+  let offSetY = 0;
+  switch (quadrant) {
+    case "TL":
+      offSetX = 0;
+      offSetY = 0;
+      break;
+    case "TR":
+      offSetX = quadSize;
+      offSetY = 0;
+      break;
+    case "BL":
+      offSetX = 0;
+      offSetY = quadSize;
+      break;
+    case "BR":
+      offSetX = quadSize;
+      offSetY = quadSize;
+      break;
+  }
+
+  MyGame.ctx.drawImage(
+    image,
+    offSetX,
+    offSetY,
+    quadSize,
+    quadSize,
+    cellSize * position[1],
+    cellSize * position[0],
+    quadSize,
+    quadSize
+  );
+}
+
 class Tomb {
   x;
   y;
@@ -112,11 +178,7 @@ class Tomb {
 }
 
 function draw() {
-  // console.log("draw");
-  // console.log(MyGame.grid.position);
-  //   console.log(grid.content);
   const canvas = document.getElementById("game-canvas")! as HTMLCanvasElement;
-  //   console.log(canvas);
   MyGame.ctx.clearRect(0, 0, canvas.width, canvas.height);
   MyGame.grid.content.forEach((row, rowIdx) => {
     row.forEach((cell, colIdx) => {
@@ -134,7 +196,8 @@ function draw() {
           cellSize,
           cellSize
         );
-      } else {
+      }
+      if (cell === 1) {
         MyGame.ctx.fillStyle = "#000000";
         MyGame.ctx.fillRect(
           cellSize * colIdx,
@@ -142,6 +205,65 @@ function draw() {
           cellSize,
           cellSize
         );
+      }
+      if (cell === 2) {
+        if (rowIdx % 2 === 0) {
+          if (colIdx % 2 === 0) {
+            // MyGame.ctx.drawImage(
+            //   footR,
+            //   0,
+            //   0,
+            //   16,
+            //   16,
+            //   cellSize * colIdx,
+            //   cellSize * rowIdx,
+            //   16,
+            //   16
+            // );
+            drawQuarterImage(footR, "TL", [rowIdx, colIdx], cellSize);
+          } else {
+            // MyGame.ctx.drawImage(
+            //   footR,
+            //   16,
+            //   0,
+            //   16,
+            //   16,
+            //   cellSize * colIdx,
+            //   cellSize * rowIdx,
+            //   16,
+            //   16
+            // );
+            drawQuarterImage(footR, "TR", [rowIdx, colIdx], cellSize);
+          }
+        } else {
+          if (colIdx % 2 === 0) {
+            // MyGame.ctx.drawImage(
+            //   footR,
+            //   0,
+            //   16,
+            //   16,
+            //   16,
+            //   cellSize * colIdx,
+            //   cellSize * rowIdx,
+            //   16,
+            //   16
+            // );
+            drawQuarterImage(footR, "BL", [rowIdx, colIdx], cellSize);
+          } else {
+            // MyGame.ctx.drawImage(
+            //   footR,
+            //   16,
+            //   16,
+            //   16,
+            //   16,
+            //   cellSize * colIdx,
+            //   cellSize * rowIdx,
+            //   16,
+            //   16
+            // );
+            drawQuarterImage(footR, "BR", [rowIdx, colIdx], cellSize);
+          }
+        }
       }
     });
   });
@@ -196,10 +318,19 @@ function initContext(): GameConfig {
 
 const MyGame = initContext();
 
+function setBlockFromOrigin(origin: Point, val: any) {
+  let pos: Point = [...origin];
+  MyGame.grid.set([...origin], val);
+  MyGame.grid.set([origin[0] + 1, origin[1]], val);
+  MyGame.grid.set([origin[0], origin[1] + 1], val);
+  MyGame.grid.set([origin[0] + 1, origin[1] + 1], val);
+}
+
 function keyboardInput() {
   window.addEventListener(
     "keydown",
     (event) => {
+      console.log("location", MyGame.grid.position);
       if (event.code === "Enter") {
         console.log("Enter");
         console.log(MyGame.stopMain);
@@ -208,22 +339,31 @@ function keyboardInput() {
       if (event.code === "ArrowUp") {
         MyGame.playerDirection = "UP";
         MyGame.grid.move4Block("UP");
+        // let pos = [...MyGame.grid.position];
+        // pos[1] += 1;
+        // MyGame.grid.set(pos, 2);
+        setBlockFromOrigin(MyGame.grid.position, 2);
         MyGame.stateChanged = true;
       }
       if (event.code === "ArrowRight") {
         MyGame.playerDirection = "RIGHT";
-
         MyGame.grid.move4Block("RIGHT");
+        // MyGame.grid.set(MyGame.grid.position, 2);
+        setBlockFromOrigin(MyGame.grid.position, 2);
         MyGame.stateChanged = true;
       }
       if (event.code === "ArrowDown") {
         MyGame.playerDirection = "DOWN";
         MyGame.grid.move4Block("DOWN");
+        // MyGame.grid.set(MyGame.grid.position, 2);
+        setBlockFromOrigin(MyGame.grid.position, 2);
         MyGame.stateChanged = true;
       }
       if (event.code === "ArrowLeft") {
         MyGame.playerDirection = "LEFT";
         MyGame.grid.move4Block("LEFT");
+        // MyGame.grid.set(MyGame.grid.position, 2);
+        setBlockFromOrigin(MyGame.grid.position, 2);
         MyGame.stateChanged = true;
       }
     },
