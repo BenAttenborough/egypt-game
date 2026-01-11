6bb0c682d128e05b8faf96f52894cce5bbcc29bf
Ben Attenborough <battenborough@gmail.com>
2026-01-11 18:38:03 +0000

    Add keyboard controls to game scheme

--- src/game/scenes/gameScene.ts
+++ src/game/scenes/gameScene.ts
@@ -1,16 +1,31 @@
 import { Player } from "../objects/player";
 
+interface keysPressed {
+    ArrowRight: boolean,
+    ArrowLeft: boolean,
+    ArrowUp: boolean,
+    ArrowDown: boolean,
+}
+
 export class GameScene {
     speed: number;
     x: number;
     x2: number;
     player: any;
+    keysPressed: keysPressed;
 
     constructor(gameConfig: any) {
         this.speed = 0.05;
         this.x = 0;
         this.x2 = 0;
         this.player = new Player(gameConfig.playerImage);
+        this.keysPressed = {
+            ArrowRight: false,
+            ArrowLeft: false,
+            ArrowUp: false,
+            ArrowDown: false,
+        }
+        this.keyboardInput();
     }
 
     update = (dt: number) => {
@@ -28,5 +43,44 @@ export class GameScene {
         ctx.fillStyle = "red";
         ctx.fillRect(this.x2, 30, 10, 10);
         this.player.drawPlayer(ctx, "DOWN", [0, 3])
+        ctx.fillText(("ArrowRight: " + this.keysPressed.ArrowRight), 10, 50)
+    }
+
+    keyboardInput = () => {
+        window.addEventListener("keydown", (e) => {
+            e.preventDefault();
+            if (e.code === "ArrowRight") {
+                this.keysPressed.ArrowRight = true;
+            }
+            if (e.code === "ArrowLeft") {
+                this.keysPressed.ArrowLeft = true;
+            }
+            if (e.code === "ArrowUp") {
+                this.keysPressed.ArrowUp = true;
+            }
+            if (e.code === "ArrowDown") {
+                this.keysPressed.ArrowDown = true;
+            }
+            // if (this.keysPressed.hasOwnProperty(e.code))
+            //   this.keysPressed[e.code] = true;
+        });
+
+        window.addEventListener("keyup", (e) => {
+            e.preventDefault();
+            if (e.code === "ArrowRight") {
+                this.keysPressed.ArrowRight = false;
+            }
+            if (e.code === "ArrowLeft") {
+                this.keysPressed.ArrowLeft = false;
+            }
+            if (e.code === "ArrowUp") {
+                this.keysPressed.ArrowUp = false;
+            }
+            if (e.code === "ArrowDown") {
+                this.keysPressed.ArrowDown = false;
+            }
+            // if (this.keysPressed.hasOwnProperty(e.code))
+            //   this.keysPressed[e.code] = false;
+        });
     }
 }
